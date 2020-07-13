e13ac2d792a5a54b1a8ec0e9028f45d7e3ad8332
Alex <gurievalex78@gmail.com>
2020-07-13 18:54:33 +0200

    List

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’,‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+
+var cognomi = ['Bianchi','Rossi','Duzioni','Balsano','Verdi'];
+var cognomeUtente = prompt('Inserisci il cognome');
+
+cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1).toLowerCase();
+// console.log(cognomeUtente);
+ cognomi.push(cognomeUtente);
+ cognomi.sort();
+
+console.log(cognomi);
+
+var posizione = cognomi.indexOf(cognomeUtente);
+console.log('La posizione "umana" ',posizione + 1);
+
+
+for(var i = 0; i < cognomi.length;i++) {
+  console.log(cognomi[i]);
+  document.getElementById('print').innerHTML +=  '<li>' + cognomi[i] + '</li>';
+}
